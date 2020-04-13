f71d616baa72003dbba8a2e966cc88bde6173e19
Toledo <gabriel.toledo@accenture.com>
2020-04-13 09:38:54 -0700

    Added shelter pins to map

--- src/components/liked-pets-page/index.jsx
+++ src/components/liked-pets-page/index.jsx
@@ -4,18 +4,32 @@ import LikedPetsMap from './liked-pets-map';
 import Footer from '../common-components/footer';
 import styles from './styles.module.scss';
 import USER_LOCATION from '../../constants/queries/userLocationQuery';
+import USERS_LIKED_PETS from '../../constants/queries/usersLikedPetsQuery';
 
-const LikedPetsPage= () => {
-  const { 
-    loading: userLocationLoading, 
-    error: userLocationError, 
-    data: userLocation 
+const LikedPetsPage = () => {
+  const {
+    loading: userLocationLoading,
+    error: userLocationError,
+    data: userLocationResponse,
   } = useQuery(USER_LOCATION);
 
+  const {
+    loading: likedPetsLoading,
+    error: likedPetsError,
+    data: likedPetsResponse,
+  } = useQuery(USERS_LIKED_PETS);
+
   let likedPetsMap;
-  if (userLocationLoading) { likedPetsMap = (<div>Loading...</div>) }; 
-  if (userLocationError) { likedPetsMap = (<div>ERROR</div>) }
-  if (userLocation) { likedPetsMap = (<LikedPetsMap  userLocation={userLocation.currentUser} />) }
+  if (userLocationLoading || likedPetsLoading) { likedPetsMap = (<div>Loading...</div>); }
+  if (userLocationError || likedPetsError) { likedPetsMap = (<div>ERROR</div>); }
+  if (userLocationResponse && likedPetsResponse) {
+    likedPetsMap = (
+      <LikedPetsMap
+        userLocation={userLocationResponse.currentUser}
+        pets={likedPetsResponse.likedPets}
+      />
+    );
+  }
   return (
     <div className={styles.Page}>
       <div className={styles.Header}>header</div>
--- src/components/liked-pets-page/liked-pets-map/index.jsx
+++ src/components/liked-pets-page/liked-pets-map/index.jsx
@@ -1,34 +1,92 @@
 import React, { useState } from 'react';
-import MapGl, { NavigationControl } from 'react-map-gl';
+import MapGl, { NavigationControl, Popup } from 'react-map-gl';
 import styles from './styles.module.scss';
-const MAPBOX_PUBLIC_TOKEN = 'pk.eyJ1Ijoid2ViYXBwcmVudGljZXRlYW0iLCJhIjoiY2s4c3ZyMW84MHI2cTNncTUyZ2RpdTZhaSJ9.wvNSyEEFLPfMzGzCOJNqrA';
-const MAP_STYLE = "mapbox://styles/webapprenticeteam/ck8sx168v01ok1jnstsaqzfhq";
+import ShelterMarker from './shelter-marker';
+import ShelterInfoDisplay from './shelter-info-display';
 
+const MAPBOX_PUBLIC_TOKEN = 'pk.eyJ1Ijoid2ViYXBwcmVudGljZXRlYW0iLCJhIjoiY2s4c3ZyMW84MHI2cTNncTUyZ2RpdTZhaSJ9.wvNSyEEFLPfMzGzCOJNqrA';
+const MAP_STYLE = 'mapbox://styles/webapprenticeteam/ck8sx168v01ok1jnstsaqzfhq';
 
 const LikedPetsMap = (props) => {
+  const { userLocation, pets, children } = props;
+  const [popupInfo, setPopupInfo] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100%',
     height: '100%',
-    latitude: props.userLocation.latitude,
-    longitude: props.userLocation.longitude,
-    zoom: 10
+    latitude: userLocation.latitude,
+    longitude: userLocation.longitude,
+    zoom: 10,
   });
 
-  return(
+  const shelters = {};
+  pets.forEach((current) => {
+    const shelterId = current.pet.shelter.id;
+    if (shelters.hasOwnProperty(shelterId)) {
+      const pet = { ...current.pet };
+      delete pet.shelter;
+      shelters[shelterId].pets.push(current.pet);
+    } else {
+      const pet = { ...current.pet };
+      shelters[shelterId] = pet.shelter;
+      delete pet.shelter;
+      shelters[shelterId].pets = [pet];
+    }
+  });
+
+  const onClickMarker = (shelterId) => {
+    setPopupInfo({ ...shelters[shelterId] });
+  };
+
+  const markers = Object.keys(shelters).map((shelterKey) => (
+    <ShelterMarker
+      key={`shelterMarker-${shelterKey}`}
+      shelterId={shelterKey}
+      latitude={parseFloat(shelters[shelterKey].latitude)}
+      longitude={parseFloat(shelters[shelterKey].longitude)}
+      numOfPets={shelters[shelterKey].pets.length}
+      onClick={onClickMarker}
+    />
+  ));
+
+  const renderShelterPopup = () => (popupInfo ? (
+    <Popup
+      className="shelter-info-popup"
+      tipSize={5}
+      anchor="left"
+      offsetLeft={40}
+      offsetTop={38}
+      longitude={parseFloat(popupInfo.longitude)}
+      latitude={parseFloat(popupInfo.latitude)}
+      closeOnClick={false}
+      onClose={() => setPopupInfo(null)}
+    >
+      <ShelterInfoDisplay
+        info={popupInfo}
+      />
+    </Popup>
+  ) : null);
+
+  return (
     <div className={styles.LikedPetsMap}>
       <MapGl
-        {...viewport}
+        width={viewport.width}
+        height={viewport.height}
+        latitude={viewport.latitude}
+        longitude={viewport.longitude}
+        zoom={viewport.zoom}
         mapboxApiAccessToken={MAPBOX_PUBLIC_TOKEN}
         onViewportChange={setViewport}
         mapStyle={MAP_STYLE}
-        children={props.children}
+        children={children}
       >
+        {markers}
+        {renderShelterPopup()}
         <div className={styles.NavControl}>
           <NavigationControl showCompass={false} />
         </div>
       </MapGl>
     </div>
-  )
-}
+  );
+};
 
 export default LikedPetsMap;
